664760fec184692729631df7f44d77e6cf61a4a4
marcos.matsuda.sp@outlook.com <marcos.matsuda.sp@outlook.com>
2019-09-23 15:33:36 -0300

    basic react redux

---
+++ basic-react-redux/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+//redux
+import { Provider } from 'react-redux';
+//Aqui é onde o estado da Store se conectará com toda a aplicação
+import { Store } from './store';
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
